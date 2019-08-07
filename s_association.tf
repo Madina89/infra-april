3f6564d6d0acfe394c1a936ee60d0d045b0a7cd5
Madina89 <madinalinux89@gmail.com>
2019-08-07 13:05:37 -0500

    updated

--- s_association.tf
+++ s_association.tf
@@ -1,4 +1,6 @@
 resource "aws_route_table_association" "a" {
-  subnet_id      = "${aws_subnet.public.id}"
+  subnet_id      = "${aws_subnet.public-a.id}"
+  subnet_id      = "${aws_subnet.public-b.id}"
+  subnet_id      = "${aws_subnet.public-c.id}"
   route_table_id = "${aws_route_table.r.id}"
 }
--- subnet.tf
+++ subnet.tf
@@ -1,31 +1,31 @@
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public-a" {
   vpc_id     = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.1.0/16"
 
   tags = {
-      Name = "${var.Name}.public"
+      Name = "${var.Name}.public-a"
       Env = "${var.Env}"
       Created_by = "${var.Created_by}"
       Dept = "${var.Dept}"
   }
 }
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public-b" {
   vpc_id     = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/16"
 
   tags = {
-      Name = "${var.Name}.public"
+      Name = "${var.Name}.public-b"
       Env = "${var.Env}"
       Created_by = "${var.Created_by}"
       Dept = "${var.Dept}"
   }
 }
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public-c" {
   vpc_id     = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.3.0/16"
 
   tags = {
-      Name = "${var.Name}.public"
+      Name = "${var.Name}.public-c"
       Env = "${var.Env}"
       Created_by = "${var.Created_by}"
       Dept = "${var.Dept}"
@@ -43,34 +43,34 @@ resource "aws_subnet" "public" {
 
 ##################### PRIVATE #########################
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private-a" {
   vpc_id     = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.11.0/24"
 
   tags = {
-      Name = "${var.Name}.private"
+      Name = "${var.Name}.private-a"
       Env = "${var.Env}"
       Created_by = "${var.Created_by}"
       Dept = "${var.Dept}"
   }
 }
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private-b" {
   vpc_id     = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.12.0/24"
 
   tags = {
-      Name = "${var.Name}.private"
+      Name = "${var.Name}.private-b"
       Env = "${var.Env}"
       Created_by = "${var.Created_by}"
       Dept = "${var.Dept}"
   }
 }
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private-c" {
   vpc_id     = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.13.0/24"
 
   tags = {
-      Name = "${var.Name}.private"
+      Name = "${var.Name}.private-c"
       Env = "${var.Env}"
       Created_by = "${var.Created_by}"
       Dept = "${var.Dept}"
