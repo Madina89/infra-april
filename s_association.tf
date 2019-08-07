d2a6e3d62bdc79af15826c04b55448cf49f3e266
Madina89 <madinalinux89@gmail.com>
2019-08-07 13:12:25 -0500

    updated

--- s_association.tf
+++ s_association.tf
@@ -1,6 +1,6 @@
 resource "aws_route_table_association" "a" {
   subnet_id      = "${aws_subnet.public-a.id}"
-  subnet_id      = "${aws_subnet.public-b.id}"
-  subnet_id      = "${aws_subnet.public-c.id}"
+  subnet_id      = "${aws_subnet.public.id}"
+  subnet_id      = "${aws_subnet.public.id}"
   route_table_id = "${aws_route_table.r.id}"
 }
